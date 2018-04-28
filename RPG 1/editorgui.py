f5f5961056943d10ac067278ac1784a820cea90c
wkpk11235 <wkpk11235@gmail.com>
2018-04-28 18:58:31 +0900

    Boi
    Boi

---
+++ RPG 1/editorgui.py
@@ -0,0 +1,10 @@
+import tkinter,pygame
+pygame.init()
+scrwidth,scrheight=800,600
+#pygame
+def create_window():
+	screen=pygame.display.set_mode((scrwidth,scrheight))
+
+#tkinter
+root=tkinter.Tk()
+root.geometry
